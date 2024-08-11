b78f40f8100c73ae3ba86df33ea1447b3d36887b
Akash Mourya <akashmourya9522@gmail.com>
2024-08-11 11:02:22 +0530

    last-one-buggy

--- src/App.jsx
+++ src/App.jsx
@@ -21,19 +21,23 @@ function App() {
     <div className="bg-black text-white h-screen  flex flex-col items-center gap-5">
       <h1 className="text-5xl">Accordion</h1>
       {
-        data.map((d)=><div className="bg-slate-500 w-96 text-lg " onClick={()=>handleMultiSelection(d.id)}>
-          <div className="bg-red-700 flex justify-around">
-          <h1 className="inline bg-orange-400"> {d.title} </h1>
-          <p className="inline bg-blue-500">+</p>
+        data.map((d)=><div key={d.id} className="bg-slate-500 w-96 text-lg hover:cursor-pointer " onClick={()=>handleMultiSelection(d.id)}>
+          <div className=" flex justify-around">
+          <h1 className="inline "> {d.title} </h1>
+          {/* <p className="inline bg-blue-500">+</p> */}
+          {
+            multiple.includes(d.id) ? <p>-</p> : <p>+</p>
+          }
           </div>
           
-
+          <div className={`transition-all duration-700 ${multiple.includes(d.id) ? `h-20`:`h-0`}`}>
           {
-            multiple.map((id)=> id===d.id ? <div className="bg-slate-700">
-              {d.content}
-            </div> : <div></div> )
-          }
-          
+            // multiple.map((id)=> id===d.id ? <div className="bg-slate-700">
+            //   {d.content}
+            // </div> : <div></div> )
+            multiple.includes(d.id) ? <div className="bg-blue-500"> {d.content} </div> : <div></div>
+           }
+          </div>
         </div>)
       }
     </div>
